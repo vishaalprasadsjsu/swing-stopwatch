72c6454fba98ee229f94c4b4eb655c0add5e0c1f
Vishaal Prasad <vishaal56@gmail.com>
2016-10-05 16:47:42 -0700

    Adding all necessary files

---
+++ AnimationTester.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+   This program implements an animation that moves
+   a stop watch.
+*/
+public class AnimationTester
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+
+      int radius = Integer.parseInt(args[0]);
+      Stopwatch stopwatch = new Stopwatch(radius);
+
+      ShapeIcon icon = new ShapeIcon(stopwatch,
+         2 * radius, 2 * radius);
+
+      JButton top = new JButton("Top");
+      top.addActionListener(
+         event -> stopwatch.topButtonPressed());
+      JButton second = new JButton("Second");
+      second.addActionListener(
+         event -> stopwatch.secondButtonPressed());
+
+      JLabel label = new JLabel(icon);
+      frame.setLayout(new FlowLayout());
+      frame.add(top);
+      frame.add(second);
+      frame.add(label);
+
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.pack();
+      frame.setVisible(true);
+
+      final int DELAY = 100;
+      // Milliseconds between timer ticks
+      Timer t = new Timer(DELAY, event ->
+         {
+            stopwatch.move();
+            label.repaint();
+         });
+      t.start();
+   }
+
+}
